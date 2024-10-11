57002a4b483e9469b8b16197f051ac334df23631
sworeman <sworeman@yandex.ru>
2024-10-11 21:44:00 +0300

    initial commit

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,12 @@
 package app.model;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class Timer {
+
 
     private Long nanoTime = System.nanoTime();
 
